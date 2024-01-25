3c9d277e45fef8c0d65c7e3da9cd493c68a0786e
Jakub Stejskal <jakubstejskal22@gmail.com>
2024-01-24 20:56:12 -0500

    LU-7064 crowdin integration

---
+++ i18n/de.ts
@@ -0,0 +1,7 @@
+export default {
+  Save_Camera_Instruction: 'Move the camera to an interesting location and click on Save Camera to save the current camera location! You can restore the cameras by click on fly to camera below',
+  Save_Camera: 'Save Camera',
+  Remove_Camera: 'Remove Camera',
+  Name: 'Name',
+  Fly_To_Camera: 'Fly to camera',
+};
